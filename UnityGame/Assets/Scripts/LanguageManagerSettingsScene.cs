2a321d1df681233108742e41dede18f56c7204b0
Danil <pppike15@gmail.com>
2021-07-26 22:09:56 +0500

    Localization fix
    Now settings absolutely localized

--- UnityGame/Assets/Scripts/LanguageManagerSettingsScene.cs
+++ UnityGame/Assets/Scripts/LanguageManagerSettingsScene.cs
@@ -1,9 +1,10 @@
 using UnityEngine;
 using UnityEngine.UI;
-
+using System.Collections.Generic;
 
 public class LanguageManagerSettingsScene : MonoBehaviour
 {
+    public Dropdown qualityDropdown;
     public Text Volume, Save, Exit, Language, Graphics, FullScreen;
     private int language;
 
@@ -21,21 +22,34 @@ public class LanguageManagerSettingsScene : MonoBehaviour
         switch (language)
         {
             case 0:
-                Volume.text = "Volume";
-                Save.text = "Save";
-                Exit.text = "Exit";
-                Language.text = "Language";
-                Graphics.text = "Graphics";
-                FullScreen.text = "Full Screen";
+                {
+                    Volume.text = "Volume";
+                    Save.text = "Save";
+                    Exit.text = "Exit";
+                    Language.text = "Language";
+                    Graphics.text = "Graphics";
+                    FullScreen.text = "Full Screen";
+
+                    //Dropdown
+                    List<string> options = new List<string>(new string[] {"Very low",
+                    "Low", "Medium", "High", "Very high", "Ultra" });
+                    qualityDropdown.AddOptions(options);
+                }
                 break;
 
             case 1:
-                Volume.text = "Громкость";
-                Save.text = "Сохранить";
-                Exit.text = "Выйти";
-                Language.text = "Язык";
-                Graphics.text = "Видео";
-                FullScreen.text = "Полный экран";
+                {
+                    Volume.text = "Громкость";
+                    Save.text = "Сохранить";
+                    Exit.text = "Выйти";
+                    Language.text = "Язык";
+                    Graphics.text = "Видео";
+                    FullScreen.text = "Полный экран";
+                    //Dropdown
+                    List<string> options = new List<string>(new string[] {"Жмыхнуло",
+                    "Второе пришествие", "Среднячок", "Класс", "Супер", "Супер класс" });
+                    qualityDropdown.AddOptions(options);
+                }
                 break;
 
         }
